51e0fd4f4e6c6fbe1835d9b224cafa322939461d
Marek Hilton <marek.hilton15@imperial.ic.ac.uk>
2021-01-15 19:46:41 +0000

    More clean up.

--- gutenmacs-fzf.el
+++ gutenmacs-fzf.el
@@ -7,26 +7,48 @@
 (require 'term)
 (require 'dash)
 
-(defconst gutenmacs-fzf-procname "gutenmacs-fzf-process")
-(defconst gutenmacs-fzf-exe "fzf")
-(defvar gutenmacs-fzf-result nil)
+
+;; Custom defintions
+
+(defcustom gutenmacs-fzf-exe
+  "fzf"
+  "Name of the fzf executable on this system."
+  :type 'string
+  :group 'gutenmacs)
+
+(defcustom gutenmacs-fzf-process-name
+  "gutenmacs-fzf-process"
+  "Name of the fzf process used by Gutenmacs."
+  :type 'string
+  :group 'gutenmacs)
+
+(defvar gutenmacs-fzf-result nil
+  "Variable where the result of an fzf search is temporarily
+stored for communication between callback and main functions.
+Intention is to remove this at some point.")
+
+
+;; gutenmacs fzf functions
 
 (defun gutenmacs-fzf-after-term-handle-exit (proc-name msg)
+  "Function called after termination of fzf process.  Used to
+temporarily populate gutenmacs-fzf-result with the result of the
+fzf search."
   (let* ((lines (split-string (buffer-string) "\n"))
 	 (selects (-filter (lambda (s) (string-match-p "\s*>\s+" s)) lines))
 	 (choice (string-trim-left (car selects) "\s*>\s+")))
-    ;(message choice)
     (setq gutenmacs-fzf-result choice)
     (kill-buffer)
     (setq unread-command-events (listify-key-sequence "\n"))))
 
 (defun gutenmacs-fzf (filename)
+  "Perform fzf search on contents of FILENAME."
   (unless (executable-find gutenmacs-fzf-exe)
     (user-error "Please ensure fzf is installed and PATH"))
   
   (advice-add 'term-handle-exit :after #'gutenmacs-fzf-after-term-handle-exit)
   (switch-to-buffer
-   (make-term gutenmacs-fzf-procname
+   (make-term gutenmacs-fzf-process-name
 	      shell-file-name nil shell-command-switch
 	      (format "cat %s | %s" filename gutenmacs-fzf-exe)))
   (term-char-mode)
@@ -47,4 +69,5 @@
 
   gutenmacs-fzf-result)
 
+(provide 'gutenmacs-fzf)
 ;;; gutenmacs-fzf.el ends here
